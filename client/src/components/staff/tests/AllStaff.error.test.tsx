e95f9609e029cc1238847ba47f5b08eedc4cb682
nivb52 <nivb52@gmail.com>
2024-06-25 18:07:51 +0300

    test/feat: custom query options & set retry false

--- client/src/components/staff/tests/AllStaff.error.test.tsx
+++ client/src/components/staff/tests/AllStaff.error.test.tsx
@@ -1,16 +1,30 @@
-import { AllStaff } from "../AllStaff";
+import { AllStaff } from '../AllStaff';
+import { http, HttpResponse } from 'msw';
+import { server } from '@/mocks/server';
 
-// import { http, HttpResponse } from "msw";
-// import { server } from '@/mocks/server';
+import { render, screen } from '@/test-utils';
+// helpers
+import { createTitle } from '@/react-query/queryClient';
 
-test("handles query error", async () => {
+test('handles query error', async () => {
+  // arrange
+  const toastTitle = createTitle('fetch', '');
+  const titlesRegex = new RegExp(toastTitle, 'i');
+  const errorcodeMentionRegex = new RegExp('500', 'i');
   // (re)set handler to return a 500 error for staff and treatments
-  // server.use(
-  // http.get("http://localhost:3030/staff", () => {
-  //   return new HttpResponse(null, { status: 500 });
-  // }),
-  // http.get("http://localhost:3030/treatments", () => {
-  //   return new HttpResponse(null, { status: 500 });
-  // })
-  // );
+  server.use(
+    http.get('http://localhost:3030/staff', () => {
+      return new HttpResponse(null, { status: 500 });
+    }),
+    http.get('http://localhost:3030/treatments', () => {
+      return new HttpResponse(null, { status: 500 });
+    })
+  );
+  // act
+  render(<AllStaff />);
+  const alertToast = await screen.findByRole('status');
+
+  // expect
+  expect(alertToast).toHaveTextContent(titlesRegex);
+  expect(alertToast).toHaveTextContent(errorcodeMentionRegex);
 });
--- client/src/react-query/queryClient.ts
+++ client/src/react-query/queryClient.ts
@@ -1,43 +1,46 @@
-import { toast } from "@/components/app/toast";
-import { MutationCache, QueryCache, QueryClient } from '@tanstack/react-query';
+import { toast } from '@/components/app/toast';
+import { MutationCache, QueryCache, QueryClient, QueryClientConfig } from '@tanstack/react-query';
 
-function createTitle(action: "fetch" | "mutation", errorMsg: string | null | undefined = "error connecting to server") {
-    return `could not ${action} data: ${errorMsg}`;
+export function createTitle(
+  action: 'fetch' | 'mutation',
+  errorMsg: string | null | undefined = 'error connecting to server'
+) {
+  return `could not ${action} data: ${errorMsg}`;
 }
 
 function errorHandler(title: string) {
-    // https://chakra-ui.com/docs/components/toast#preventing-duplicate-toast
-    // one message per page load, not one message per query
-    // the user doesn't care that there were three failed queries (e.g" on the staff page  (staff, treatments, user))
-    const id = "react-query-toast";
-    if (!toast.isActive(id)) {
-        toast({ id, title, status: "error", variant: "subtle", isClosable: true });
-    }
+  // https://chakra-ui.com/docs/components/toast#preventing-duplicate-toast
+  // one message per page load, not one message per query
+  // the user doesn't care that there were three failed queries (e.g" on the staff page  (staff, treatments, user))
+  const id = 'react-query-toast';
+  if (!toast.isActive(id)) {
+    toast({ id, title, status: 'error', variant: 'subtle', isClosable: true });
+  }
 }
 
 function mutationErrorHandler(errorMsg: string) {
-    errorHandler(createTitle("mutation", errorMsg))
+  errorHandler(createTitle('mutation', errorMsg));
 }
 
 function queryErrorHandler(errorMsg: string) {
-    errorHandler(createTitle("fetch", errorMsg))
+  errorHandler(createTitle('fetch', errorMsg));
 }
 
-
 /////////////// MAIN ///////////////////////
-export const queryClient = new QueryClient({
-    queryCache: new QueryCache({
-        onError: (err) => queryErrorHandler(err.message)
-    }),
-    mutationCache: new MutationCache({
-        onError: (err) => mutationErrorHandler(err.message)
-    }),
-    defaultOptions: {
-        queries: {
-            staleTime: 1000 * 60 * 10, // 10 minutes
-            gcTime: 1000 * 60 * 15, // 15 minutes
-            // good advice is not to define it here but increase the stale time
-            refetchOnWindowFocus: false
-        }
+export const queryClientOptions: QueryClientConfig = {
+  queryCache: new QueryCache({
+    onError: (err) => queryErrorHandler(err.message)
+  }),
+  mutationCache: new MutationCache({
+    onError: (err) => mutationErrorHandler(err.message)
+  }),
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 10, // 10 minutes
+      gcTime: 1000 * 60 * 15, // 15 minutes
+      // good advice is not to define it here but increase the stale time
+      refetchOnWindowFocus: false
     }
-});
+  }
+};
+export const queryClient = new QueryClient(queryClientOptions);
